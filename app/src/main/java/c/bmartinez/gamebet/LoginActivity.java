dd1e0db4260476d4557d4f7bc52e8614b7d44bff
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-05-13 01:05:50 -0700

    login icon added

---
+++ app/src/main/java/c/bmartinez/gamebet/LoginActivity.java
@@ -0,0 +1,10 @@
+package c.bmartinez.gamebet;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+
+}
